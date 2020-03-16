022838c2ee000953e61423c82505462630a46ff0
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-03-15 22:18:54 -0400

    minor clean up.

--- scripts/bca/import.sh
+++ scripts/bca/import.sh
@@ -7,14 +7,14 @@ psql -c "create schema if not exists ${working_schema}"
 psql_w_envs cancer/prepare_reference.sql
 
 # prepare patient data
+#psql_w_envs cancer/prepare_vital.sql #! divide by zero error
 psql_w_envs cancer/prepare_cohort.sql
 psql_w_envs cancer/prepare_diagnosis.sql
 psql_w_envs cancer/prepare_performance.sql
 psql_w_envs cancer/prepare_lab.sql
 psql_w_envs cancer/prepare_lot.sql
-psql_w_envs cancer/prepare_stage.sql #! to be updated with TNM for BCA
+psql_w_envs cancer/prepare_stage.sql
 psql_w_envs cancer/prepare_histology.sql
-#psql_w_envs cancer/prepare_vital.sql #! divide by zero error
 psql_w_envs cancer/prepare_variant.sql
 psql_w_envs cancer/prepare_biomarker.sql
 #psql_w_envs caregiver/icd_physician.sql
@@ -27,13 +27,13 @@ psql_w_envs cancer/prepare_attribute.sql
     #to rescue stage using TNM c/p
 
 # perform the attribute matching
-psql_w_envs cancer/match_icd.sql
 #psql_w_envs cancer/match_loinc.sql
+psql_w_envs cancer/match_icd.sql
 psql_w_envs cancer/match_aof20200311.sql #update match_aof.. later
 psql_w_envs cancer/match_rxnorm_wo_modality.sql #update match_rxnorm later
 psql_w_envs bca/prepare_misc_measurement.sql #mv to cancer later
 psql_w_envs cancer/match_misc_measurement.sql
-psql_w_envs bca/prepare_cat_measurement.sql
+psql_w_envs bca/prepare_cat_measurement.sql #menopausal to be cleaned
 psql_w_envs bca/match_cat_measurement.sql #mv to cancer later
 psql_w_envs cancer/match_icdo_rex.sql
 psql_w_envs cancer/match_stage.sql
@@ -49,7 +49,7 @@ cd "${working_dir}"
 select_from_db_schema_table.py rimsdw ${working_schema}.v_master_sheet > v_master_sheet_$(today_stamp).csv
 select_from_db_schema_table.py rimsdw ${working_schema}.v_crit_attribute_used > v_crit_attribute_used_$(today_stamp).csv
 select_from_db_schema_table.py rimsdw ${working_schema}.v_demo_w_zip > v_demo_w_zip_$(today_stamp).csv
-select_from_db_schema_table.py rimsdw ${working_schema}.v_treating_physician > v_treating_physician_$(today_stamp).csv
+#select_from_db_schema_table.py rimsdw ${working_schema}.v_treating_physician > v_treating_physician_$(today_stamp).csv
 
 # match to patients (to be updated)
 #psql_w_envs cancer/master_patient.sql #> trial2patients
