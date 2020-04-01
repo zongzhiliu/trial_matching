86e1b6410bbed15e3b0047a4eace8c72faa03061
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-04-01 15:26:21 -0400

    deliver caregiver_id instead of name

--- scripts/deliver_treating_physicians.sh
+++ scripts/deliver_treating_physicians.sh
@@ -1,14 +1,13 @@
 cd /Users/zongzhiliu/Sema4/rdmsdw/ct
 mask_magic=3040
-cancer=CRC
 function etl_treating_physicians { cancer=$1
     select_from_db_schema_table.py rimsdw -q "
             select person_id+${mask_magic} as person_id
-            , caregiver, zip_code, num_visits
+            , caregiver_id, zip_code, num_visits
             from ct_${cancer}.treating_physicians
             order by person_id" > \
         ${cancer}.treating_physicians.csv
-    load_into_db_schema_some_csvs.py pharma db_data_bridge \
+    load_into_db_schema_some_csvs.py -d pharma db_data_bridge \
         ${cancer}.treating_physicians.csv
 }
 etl_treating_physicians NSCLC
