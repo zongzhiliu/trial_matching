396f55d60f139d6eabaef883ef5e849599c6cd0a
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-04-08 17:56:54 -0400

    quickfix: rm attributes with only null matches

--- scripts/ct_PCA/master_match.sql
+++ scripts/ct_PCA/master_match.sql
@@ -40,12 +40,28 @@ join crit_attribute_used using (attribute_id)
 order by person_id, attribute_id limit 99;
 */
 
-drop view if exists _master_match;
+drop view if exists _master_match cascade;
 create view _master_match as
-select attribute_id, trial_id, person_id, patient_value::varchar, match
-from _p_a_t_match
-union all
-select attribute_id, trial_id, person_id, patient_value::varchar, match
-from _p_a_match join trial_attribute_used using (attribute_id)
+with mm as (
+    select attribute_id, trial_id, person_id, patient_value::varchar, match
+    from _p_a_t_match
+    union all
+    select attribute_id, trial_id, person_id, patient_value::varchar, match
+    from _p_a_match join trial_attribute_used using (attribute_id)
+)
+-- quickfix: remove attributes with only null matches
+, a_good as (
+    select attribute_id
+    , bool_or(match is not null) good
+    from mm
+    group by attribute_id
+    having (good)
+)
+select mm.*
+from mm join a_good using (attribute_id)
 ;
-
+/*
+select count(*), count(distinct attribute_id) from _master_match;
+    --31216668 |   159
+    --28567046 |   143
+*/
