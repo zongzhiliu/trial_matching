86cc85ba14764f1b8d213ae842047ba04edac153
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-04-09 15:39:03 -0400

    load the old attribute with mandatory and logic rules

--- scripts/ct_PCA/setup.sql
+++ scripts/ct_PCA/setup.sql
@@ -6,15 +6,11 @@ Results:
     ref_drug_mapping
     ref_lab_mapping
     ref_histology_mapping
-settings:
-    @set cancer_type=PCA
-    @set cancer_type_icd=^(C61|185)
+load_into_db_schema_some_csvs.py rimsdw ct crit_attribute_used_lca_pca_20200408.csv -d
 */
---set search_path=ct_${cancer_type};
---show search_path;
--- settings
 create or replace view _crit_attribute_raw as
-select * from ct.crit_attribute_raw_20200223;
+--select * from ct.crit_attribute_raw_20200223;
+select * from ct.crit_attribute_used_lca_pca_20200408;
 
 create or replace view _trial_attribute_raw as
 select * from trial_attribute_raw_20200223;
@@ -53,21 +49,27 @@ select attribute_id, ie_values, a.*
 from tmp join crit_attribute_raw a using (attribute_id)
 order by attribute_id
 ;
+*/
 -- crit_attribute_used
+--alter table crit_attribute_used rename to crit_attribute_used_old_20200408;
 drop table if exists crit_attribute_used cascade;
 create table crit_attribute_used as
-select crit_id, crit_name, mandated
-, attribute_id, c.attribute_group, c.attribute_name, c.value
+select attribute_id, attribute_group, attribute_name, attribute_value
+, mandatory_default, logic_default
 from _crit_attribute_raw c
 join (select distinct attribute_id
     from trial_attribute_used) using (attribute_id)
 ;
+
+--drop view v_crit_attribute_used;
 create or replace view v_crit_attribute_used as
-select attribute_id, attribute_group, attribute_name, value, mandated, crit_id
+select attribute_id, attribute_group, attribute_name, attribute_value
+, mandatory_default, logic_default
 from crit_attribute_used
 order by attribute_id
 ;
 /*qc
 select count(*) from crit_attribute_used;
     --170
+    --183
 */
