9401e8ff637955f89c52e59166c509880fd7c1b5
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-04-23 22:51:15 -0400

    export table/view with a datestamp as a function

--- scripts/util/util.sh
+++ scripts/util/util.sh
@@ -7,6 +7,11 @@ function psql_w_envs {
     | psql --echo-all --no-psqlrc -v ON_ERROR_STOP=1
 }
 
+# export a table/view form working_schema to working_dir with today_stamp
+function export_w_today { tv=$1
+    select_from_db_schema_table.py ${db_conn} ${working_schema}.${tv} > ${working_dir}/${tv}_$(today_stamp).csv
+}
+
 function mysql_w_envs {
     cat $1 \
     | substitute_env_vars_in_pipe.py \
