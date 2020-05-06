47afaadd32cbade499c82516849ea0520fcd6f42
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-05-05 23:47:27 -0400

    all_gene_alteration created

--- scripts/viecure/viecure_ct/gen_others.sql
+++ scripts/viecure/viecure_ct/gen_others.sql
@@ -9,3 +9,19 @@ join tumor_marker_list tml on tumor_marker_id=tml.id --good
 where not nvl(inactive, False) 
     and not nvl(errored, False)
 ;
+
+set search_path=viecure_emr;
+create table viecure_ct.all_gene_alteration as
+select patientid person_id, genelist.name gene_name
+, mutation_type.description mutation_type_name
+, gene_source_type.description gene_source_type_name
+, created_tstamp test_time
+, raw_position
+, gd.pd_result
+, gd.pd_tumor_score_percent
+from patient_gene_details gd
+join patient_gene on patient_gene_id=patient_gene.id
+join genelist on genelist_id=genelist.id
+join mutation_type on mutation_type_id=mutation_type.id
+join gene_source_type on gene_source_type_id=gene_source_type.id
+;
