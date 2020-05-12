0c024fef2778b800ca5ebfc7c894e069052964e9
Mingwei Zhang <mingwei.zhang@sema4genomics.com>
2020-05-11 21:23:11 -0400

    bug found (karnosky -> karnofsky)

--- scripts/cancer/prepare_misc_measurement.sql
+++ scripts/cancer/prepare_misc_measurement.sql
@@ -4,7 +4,7 @@ create table misc_measurement as (
     from demo
     union select person_id, 'ecog', ecog_ps
     from latest_ecog
-    union select person_id, 'karnosky', karnofsky_pct
+    union select person_id, 'karnofsky', karnofsky_pct
     from latest_karnofsky
     union select person_id, 'lot', n_lot
     from lot
