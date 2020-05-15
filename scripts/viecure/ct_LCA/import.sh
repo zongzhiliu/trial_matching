916f2c16c5129d6e03a02dee72e1db5bf9d0e544
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-05-15 18:47:28 -0400

    minor

--- scripts/viecure/ct_LCA/import.sh
+++ scripts/viecure/ct_LCA/import.sh
@@ -5,26 +5,23 @@ pgsetup $db_conn
 psql_w_envs viecure/ct_LCA/setup.sql
 psql_w_envs viecure/ct_lca/match_test.sql
 psql_w_envs viecure/ct_LCA/prepare_cohort.sql
-############################################################ next
 psql_w_envs viecure/ct_LCA/match_dx.sql --ct_NSCLC/match_code_icd.sql # > pa_icd_rex
 
-#psql_w_envs cancer/prepare_lot.sql
-#psql_w_envs cancer/prepare_variant.sql
-#psql_w_envs cancer/prepare_biomarker.sql
-#psql_w_envs caregiver/icd_physician.sql
 
 # match to attributes
 psql_w_envs cancer/match_attributes__age.sql #> pat_age
 psql_w_envs cancer/match_attributes__stage.sql #>_pa_stage
 psql_w_envs ct_NSCLC/match_attributes__histology.sql  #> pa_histology
 
-psql_w_envs cancer/match_attributes__performance.sql #>_pa_ecog/karnofsky
-
-#load attribute table with code columns
-# psql_w_envs cancer/match_attributes__lot.sql #>_pa_lot
+############################################################ next
+# psql_w_envs ct_NSCLC/match_code_drug.sql #> pa_drug
+# psql_w_envs cancer/match_attributes__performance.sql #>_pa_ecog/karnofsky
+#psql_w_envs cancer/prepare_variant.sql
+#psql_w_envs cancer/prepare_biomarker.sql
+#psql_w_envs caregiver/icd_physician.sql
 # psql_w_envs ct_NSCLC/match_code_variant.sql #> pa_variant
 # psql_w_envs ct_NSCLC/match_code_biomarker.sql #> pa_biomarker
-# psql_w_envs ct_NSCLC/match_code_drug.sql #> pa_drug
+
 # compile the matches
 psql_w_envs ct_NSCLC/master_match.sql  #> master_match
 psql_w_envs ct_NSCLC/update_attributes.sql #> crit/trial_attribute_updated
