0886b08321fe8ba9bb3e6264256ff4e5d552a5a2
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-05-15 21:23:51 -0400

    quickfix

--- scripts/viecure/ct_LCA/prepare_cohort.sql
+++ scripts/viecure/ct_LCA/prepare_cohort.sql
@@ -9,7 +9,7 @@ drop table if exists cohort cascade;
 create table cohort as
 select distinct person_id
 from viecure_ct.cancer_dx cd
-join viecure_ct.demo_plus d
+join viecure_ct.demo_plus d using (person_id)
 where cancer_type_name=${cancer_type}
     and date_of_death is NULL
     and datediff(day, last_visit_date, '${protocal_date}')/365.25 <= ${last_visit_within}
