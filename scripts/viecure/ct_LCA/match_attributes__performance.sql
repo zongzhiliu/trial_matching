96518c5fe1c6f523ed33c46b0e69ec24ccae3232
Mingwei Zhang <mingwei.zhang@sema4genomics.com>
2020-05-20 17:52:37 -0400

    match performance

--- scripts/viecure/ct_LCA/match_attributes__performance.sql
+++ scripts/viecure/ct_LCA/match_attributes__performance.sql
@@ -4,3 +4,17 @@ Requires: demo (date_of_birth)
 , trial_attribute_used (ie_value)
 Result: _pat_performance
 */
+set search_path to ct_LCA;
+CREATE TABLE _p_a_performance AS
+SELECT person_id, attribute_id, 
+CASE code_ext
+	WHEN 'eq' then score = attribute_value
+	END AS MATCH
+FROM viecure_ct.assessment a 
+JOIN crit_attribute_used on code = lower(assessment_type)
+JOIN cohort using(person_id)
+
+select (*)
+FROM cohort 
+JOIN viecure_ct.assessment using(person_id)
+WHERE lower(assessment_type) = 'ecog' or lower(assessment_type) = 'karnofsky'
