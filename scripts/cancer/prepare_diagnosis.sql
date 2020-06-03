c8ce65c2be60633f5ce79d0b0e434c180cca3bf3
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-06-03 12:47:22 -0400

    rm description from laster_icd

--- scripts/cancer/prepare_diagnosis.sql
+++ scripts/cancer/prepare_diagnosis.sql
@@ -7,17 +7,17 @@ Results:
 drop table if exists latest_icd;
 create table latest_icd as
 with _all_dx as (
-    select distinct person_id, dx_date, icd, icd_code, description
+    select distinct person_id, dx_date, icd, icd_code
     from (select medical_record_number mrn, dx_date
-        , icd, context_diagnosis_code icd_code, description
+        , icd, context_diagnosis_code icd_code
         from dev_patient_info_${cancer_type}.all_diagnosis) d
     join cplus_from_aplus.person_mrns using (mrn)
     join cohort using (person_id)
 )
-select person_id, icd_code, icd as context_name, description, dx_date
+select person_id, icd_code, icd as context_name, dx_date
 from (select *, row_number() over (
         partition by person_id, icd_code
-        order by dx_date desc nulls last, description)
+        order by dx_date desc nulls last)
     from _all_dx
     )
 where row_number=1
