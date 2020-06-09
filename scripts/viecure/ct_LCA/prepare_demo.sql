bb169b5423301f0a5f9cb8b86934e6563734ca3b
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-06-09 16:23:22 -0400

    add v_demo

--- scripts/viecure/ct_LCA/prepare_demo.sql
+++ scripts/viecure/ct_LCA/prepare_demo.sql
@@ -11,3 +11,15 @@ join viecure_ct.demo_plus using (person_id)
 ;
 --qc
 select count(distinct person_id) from demo; -- 1092
+
+drop view if exists v_demo;
+create view v_demo as select person_id
+, decode(gender_name, 'F', 'Female', 'M', 'Male') gender_name
+, date_trunc('month', date_of_birth) date_of_birth_truncated
+, race_name
+, ethnicity_name
+, last_visit_date::date
+from demo
+order by last_visit_date desc nulls last
+;
+
