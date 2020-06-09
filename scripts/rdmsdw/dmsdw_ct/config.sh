26c135ff70e4769e0c8a251a85e4c2b77b8c137e
Zongzhi Zachary Liu <zongzhi.liu@sema4genomics.com>
2020-06-09 13:32:21 -0400

    misc addtion

---
+++ scripts/rdmsdw/dmsdw_ct/config.sh
@@ -0,0 +1,8 @@
+export working_schema="dmsdw_ct"
+export dmsdw=dmsdw_2019q1
+export ref_drug_mapping=ct.drug_mapping_cat_expn6
+export ref_lab_mapping=ct.ref_lab_loinc_mapping
+export ref_proc_mapping=ct.ref_proc_mapping_20200325
+export ref_rx_mapping=ct.ref_rx_mapping_20200325
+
+export working_dir="$HOME/Sema4/rdmsdw/${working_schema}"
